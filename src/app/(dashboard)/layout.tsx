31bbf6d15bb99a4ce12d375b96f36dc29d8f8f04
mucahittasan <mucahittasan0@gmail.com>
2025-10-02 12:51:31 +0300

    feat(builder): implement and style all multi-step form UIs
    Created and styled the UI for all steps of the portfolio builder (Profile, Experience, Education,
    Projects, Skills) using a dynamic, inline form structure.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -14,8 +14,9 @@ export default async function DashboardLayout({
   }
 
   return (
-    <div className="flex min-h-screen">
-      <Sidebar />
+    <div className="relative flex min-h-screen">
+      <Sidebar user={session.user} />
+      <div className="absolute bottom-[0%] right-[10%] h-[200px] w-[200px] bg-gradient-to-tr from-[#9c40ff] to-[#ffaa40] blur-[220px]" />{' '}
       <main className="relative flex-1 p-8">
         <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] dark:bg-black dark:bg-[radial-gradient(#ffffff33_1px,transparent_1px)]"></div>
         {children}
