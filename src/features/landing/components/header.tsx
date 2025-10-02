9f66f050cae8342dbbdb07f72da92d6d1bf7ddf2
mucahittasan <mucahittasan0@gmail.com>
2025-10-02 12:03:45 +0300

    feat(auth): implement session management and protected routes

--- src/features/landing/components/header.tsx
+++ src/features/landing/components/header.tsx
@@ -1,8 +1,12 @@
-import { Leaf } from 'lucide-react';
+import { Leaf, ArrowRightFromLine, LogOut } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { auth } from '@/auth';
+import { logout } from '@/features/auth/actions/logout';
 
-export function Header() {
+export async function Header() {
+  const session = await auth();
+
   return (
     <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
@@ -17,13 +21,30 @@ export function Header() {
           <Button asChild variant="ghost">
             <Link href="/#pricing">Pricing</Link>
           </Button>
-
-          <Button
-            className="bg-foreground text-background hover:bg-foreground/90"
-            asChild
-          >
-            <Link href="/register">Get Started</Link>
-          </Button>
+          {session?.user ? (
+            <div className="flex items-center gap-2">
+              <Button
+                className="bg-foreground text-background hover:bg-foreground/90"
+                asChild
+              >
+                <Link href="/dashboard">Dashboard</Link>
+              </Button>
+              <Button
+                className="border border-red-500 hover:bg-white hover:text-red-500"
+                onClick={logout}
+                variant="destructive"
+              >
+                <LogOut className="h-4 w-4" />
+              </Button>
+            </div>
+          ) : (
+            <Button
+              className="bg-foreground text-background hover:bg-foreground/90"
+              asChild
+            >
+              <Link href="/register">Get Started</Link>
+            </Button>
+          )}
         </nav>
       </div>
     </header>
